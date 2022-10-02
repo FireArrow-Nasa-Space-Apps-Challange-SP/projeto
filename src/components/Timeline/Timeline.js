cb7da10e9106179f41d79df00478db7e31fbf254
TechioRafael <110608373+RafaelTechio@users.noreply.github.com>
2022-10-02 01:26:18 -0300

    FEAT: subindo as coisas

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useHistory } from 'react';
 import styles from './Timeline.module.css'
 import TimelineItem from './TimelineItem'
 
 function Timeline() {
     const [currentSectionId, setCurrentSectionId] = useState(false);
+    const history = useHistory();
 
     const getAllSections = () => {
         return [
@@ -41,6 +42,10 @@ function Timeline() {
         const sectionOffsets = getAllSections()
 
         const currentSectionOffset = sectionOffsets.find(sectionOffset => {
+            const anchor = window.location.hash.substr(1)
+            if(anchor){
+               history.pushState({}, null, window.location.href.replace(window.location.hash, ''));
+            }
             return window.location.hash.substr(1) === sectionOffset.id || (scrollTop > sectionOffset.top && scrollTop < sectionOffset.bottom)
         })
 
@@ -54,12 +59,12 @@ function Timeline() {
     })
 
     const sections = [
-        {name:"LandSat7", date: "28/02/2003", id: 'section1'},
-        {name:"Terra", date: "30/04/2015", id: 'section2'},
-        // {name:"Aqua", date: "28/02/2003", id: 'section3'},
-        // {name:"Aura", date: "28/02/2003", id: 'section4'},
-        // {name:"CloudSat", date: "28/02/2003", id: 'section5'},
-        // {name:"Calipso", date: "28/02/2003", id: 'section6'},
+        {name:"LandSat7", date: "15/04/99", id: 'section1'},
+        {name:"Terra", date: "18/12/99", id: 'section2'},
+        {name:"Aqua", date: "4/05/02", id: 'section3'},
+        {name:"Aura", date: "28/02/2003", id: 'section4'},
+        {name:"CloudSat", date: "28/02/2003", id: 'section5'},
+        {name:"Calipso", date: "28/02/2003", id: 'section6'},
     ]
 
     return (
